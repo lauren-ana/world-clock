cdaf5324dfd5f6e3eab6a73dc412ebcde7ab0a25
lauren-ana <lauren_anahera@hotmail.com>
2026-05-29 08:29:07 +1000

    Finalised formatting world clock JS code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-let sydneyDateElement = document.querySelector("#sydneyDateElement");
-let sydneyTimeElement = document.querySelector("#sydneyTimeElement");
-sydneyDateElement.innerHTML = moment().format("dddd Do MMMM YYYY");
-sydneyTimeElement.innerHTML = moment()
-  .tz("Australia/Sydney")
-  .format("h:mm:ss A");
+function updateTime() {
+  let time = moment();
+
+  let sydneyDateElement = document.querySelector("#sydneyDateElement");
+  let sydneyTimeElement = document.querySelector("#sydneyTimeElement");
+  sydneyDateElement.innerHTML = time.format("dddd Do MMMM YYYY");
+  sydneyTimeElement.innerHTML = `${time.tz("Australia/Sydney").format("h:mm:ss")} <span class="AM-PM">${time.format("A")}</span>`;
+
+  let losAngelesDateElement = document.querySelector("#losAngelesDateElement");
+  let losAngelesTimeElement = document.querySelector("#losAngelesTimeElement");
+  losAngelesDateElement.innerHTML = time.format("dddd Do MMMM YYYY");
+  losAngelesTimeElement.innerHTML = `${time.tz("America/Los_Angeles").format("h:mm:ss")} <span class="AM-PM">${time.format("A")}</span>`;
+
+  let tokyoyDateElement = document.querySelector("#tokyoDateElement");
+  let tokyoTimeElement = document.querySelector("#tokyoTimeElement");
+  tokyoDateElement.innerHTML = time.format("dddd Do MMMM YYYY");
+  tokyoTimeElement.innerHTML = `${time.tz("Asia/Tokyo").format("h:mm:ss")} <span class="AM-PM">${time.format("A")}</span>`;
+
+  let parisDateElement = document.querySelector("#parisDateElement");
+  let parisTimeElement = document.querySelector("#parisTimeElement");
+  parisDateElement.innerHTML = time.format("dddd Do MMMM YYYY");
+  parisTimeElement.innerHTML = `${time.tz("Europe/Paris").format("h:mm:ss")} <span class="AM-PM">${time.format("A")}</span>`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
