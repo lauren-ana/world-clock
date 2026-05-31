1354f4a637bd46b76c8bd854ee995a46028510c1
lauren-ana <lauren_anahera@hotmail.com>
2026-05-31 14:42:02 +1000

    Created citySelect function to class add/remove classes and highlight selected city

--- src/index.js
+++ src/index.js
@@ -1,26 +1,45 @@
 function updateTime() {
   let time = moment();
 
+  let sydneyTime = moment().tz("Australia/Sydney");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+  let parisTime = moment().tz("Europe/Paris");
+
   let sydneyDateElement = document.querySelector("#sydneyDateElement");
   let sydneyTimeElement = document.querySelector("#sydneyTimeElement");
   sydneyDateElement.innerHTML = time.format("dddd Do MMMM YYYY");
-  sydneyTimeElement.innerHTML = `${time.tz("Australia/Sydney").format("h:mm:ss")} <span class="AM-PM">${time.format("A")}</span>`;
+  sydneyTimeElement.innerHTML = `${sydneyTime.format("h:mm:ss")} <span class="AM-PM">${time.format("A")}</span>`;
 
   let losAngelesDateElement = document.querySelector("#losAngelesDateElement");
   let losAngelesTimeElement = document.querySelector("#losAngelesTimeElement");
   losAngelesDateElement.innerHTML = time.format("dddd Do MMMM YYYY");
-  losAngelesTimeElement.innerHTML = `${time.tz("America/Los_Angeles").format("h:mm:ss")} <span class="AM-PM">${time.format("A")}</span>`;
+  losAngelesTimeElement.innerHTML = `${losAngelesTime.format("h:mm:ss")} <span class="AM-PM">${time.format("A")}</span>`;
 
-  let tokyoyDateElement = document.querySelector("#tokyoDateElement");
+  let tokyoDateElement = document.querySelector("#tokyoDateElement");
   let tokyoTimeElement = document.querySelector("#tokyoTimeElement");
   tokyoDateElement.innerHTML = time.format("dddd Do MMMM YYYY");
-  tokyoTimeElement.innerHTML = `${time.tz("Asia/Tokyo").format("h:mm:ss")} <span class="AM-PM">${time.format("A")}</span>`;
+  tokyoTimeElement.innerHTML = `${tokyoTime.format("h:mm:ss")} <span class="AM-PM">${time.format("A")}</span>`;
 
   let parisDateElement = document.querySelector("#parisDateElement");
   let parisTimeElement = document.querySelector("#parisTimeElement");
   parisDateElement.innerHTML = time.format("dddd Do MMMM YYYY");
-  parisTimeElement.innerHTML = `${time.tz("Europe/Paris").format("h:mm:ss")} <span class="AM-PM">${time.format("A")}</span>`;
+  parisTimeElement.innerHTML = `${parisTime.format("h:mm:ss")} <span class="AM-PM">${time.format("A")}</span>`;
 }
+
+let citySelect = document.querySelector("#city");
+citySelect.addEventListener("change", function () {
+  let selectedValue = citySelect.value;
+
+  document.querySelectorAll(".display-time").forEach(function (div) {
+    div.classList.remove("selected");
+  });
+
+  let matchingDiv = document.querySelector(`#${selectedValue}`);
+  if (matchingDiv) {
+    matchingDiv.classList.add("selected");
+  }
+});
 
 updateTime();
 setInterval(updateTime, 1000);
